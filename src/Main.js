bfbde7a15b2b650efd0806330b42dbd8e70fb699
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-07-03 14:34:58 -0400

    Add ChatHeader component

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+
 class Main extends Component {
     render() {
         return (
-            <div className="Main">
-                Main
+            <div className="Main" style={styles}>
+                <Sidebar />
+                <Chat />
             </div>
         )
     }
+}
+
+const styles = {
+    //put css rules here
+    display: 'flex',
+    alignItems: 'stretch', //full height of container
+    height: '100vh', //entire height of window
 }
 
 export default Main
