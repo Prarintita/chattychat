7a5fb38a3332d2a221ea43eca5863b29b7b847c5
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-07-03 15:31:33 -0400

    Add messages to chat state

--- src/Chat.js
+++ src/Chat.js
@@ -5,11 +5,39 @@ import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
 class Chat extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            messages: [
+                {
+                    id: 1, 
+                    user: {
+                        uid: 'skadnwjndaj', 
+                        displayName: 'Ada', 
+                        email: 'ada@haha.com',
+                        },
+                    body: 'chatchat',
+                },
+        
+                {
+                    id: 2,
+                    user: {
+                        uid: 'ajdwnadaw',
+                        displayName: 'Seeek',
+                        email: 'seek@kaka.com',
+                    },
+                    body: 'This is great!',
+                },
+            ]
+        
+        }
+    }
     render() {
         return (
             <div className="Chat">
                 <ChatHeader />
-                <MessageList />
+                <MessageList messages={this.state.messages}/>
                 <MessageForm />
             </div>
         )
--- src/MessageList.js
+++ src/MessageList.js
@@ -2,34 +2,12 @@ import React from 'react'
 
 import Message from './Message'
 
-const MessageList = () => {
-    const messages = [
-        {
-            id: 1, 
-            user: {
-                uid: 'skadnwjndaj', 
-                displayName: 'Ada', 
-                email: 'ada@haha.com',
-                },
-            body: 'chatchat',
-        },
-
-        {
-            id: 2,
-            user: {
-                uid: 'ajdwnadaw',
-                displayName: 'Seeek',
-                email: 'seek@kaka.com',
-            },
-            body: 'This is great!',
-        },
-    ]
-
+const MessageList = (props) => {
     return (
         <div className="MessageList">
             {
-                messages.map(msg => 
-                    <Message message={msg} key={msg.id} />    
+                props.messages.map(msg => 
+                (<Message message={msg} key={msg.id} />)
                 )
             }
         </div>
