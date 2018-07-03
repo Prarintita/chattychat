4b92198a65fe9977f3b7c443a32bd6f4209a209d
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-07-03 16:37:36 -0400

    Style sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,44 @@
 import React from 'react'
 
-const Sidebar = () => {}
+const Sidebar = () => {
     return (
-        <div className="Sidebar">
-            {props.user.displayName}
+        <aside className="Sidebar" style={styles.sidebar}>
+        <div className="UserInfo">
+            <div className="Avatar"></div>
+            <div className="user">{props.user.displayName}</div>
+            <a href="#">
+            <i className="fas fa-sign-out-alt"></i>
+            </a>
         </div>
+        <h1>XTBC 18</h1>
+        <nav className="RoomList">
+            <h2>Rooms</h2>
+            <ul>
+            <li><a href="#">general</a></li>
+            <li><a href="#">random</a></li>
+            </ul>
+        </nav>
+        </aside>
     )
+}
+
+const styles = {
+    sidebar: {
+        backgroundColor: '#333344',
+        color: 'rgba(255, 255, 255, 0.8)',
+        width: '12rem',
+        padding: '1rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+
+    children: {
+
+    },
+
+    h1: {
+
+    },
 }
 
 export default Sidebar
