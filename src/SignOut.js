faa50b58a21ad250caca3c070b1747b55cdb5c99
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-07-09 13:44:28 -0400

    Reformat code to look nicer

--- src/SignOut.js
+++ src/SignOut.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
-
+import React from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
-const SignOut = ({signOut}) => {
-    return (
-        <button
-            className={css(styles.button)} //gives right className back
-            onCLick={signOut}
-        >
-            <i className="fas fa-sign-out-alt"></i>
-        </button>
-    )
+const SignOut = ({ signOut }) => {
+  return (
+    <button
+      className={css(styles.button)}
+      onClick={signOut}
+    >
+      <i className="fas fa-sign-out-alt"></i>
+    </button>
+  )
 }
-const styles = StyleSheet.create({
-    button: {
-        border: '0',
-        padding: '0',
-        backgroundColor: 'transparent',
-        color: 'rgba(255, 255, 255, 0.6)',
-        fontSize: '1.2rem',
-        transition: 'color 0.25s ease-out',
-        cursor: 'pointer',
 
-        ':hover': {
-            color: 'white',
-        },
-    },
+const styles = StyleSheet.create({
+  button: {
+    border: 0,
+    padding: 0,
+    backgroundColor: 'transparent',
+    color: 'rgba(255, 255, 255, 0.6)',
+    fontSize: '1.2rem',
+    transition: 'color 0.25s ease-out',
+    cursor: 'pointer',
+
+    ':hover': {
+      color: 'white',
+    }
+  },
 })
 
 export default SignOut
