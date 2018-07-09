fa1cfdf5d8bb307b0f1fb71873dd69831cee4793
Prarintita Ratanavarinchai <prarintita@gmail.com>
2018-07-09 16:34:34 -0400

    Add function to change rooms

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,40 @@ import Sidebar from './Sidebar'
 import Chat from './Chat'
 
 class Main extends Component {
+    state = {
+        room: {
+            name: 's3afternoon',
+            desc: 'Ask questions and share code',
+        },
+
+        rooms: {
+            s3afternoon: {
+                name: 's3afternoon',
+                desc: 'Ask questions and share code',
+            },
+
+            general: {
+                name: 'general',
+                desc: 'Chat about whatever',
+            },
+
+            random: {
+                name: 'random',
+                desc: 'cat GIFS, etc.',
+            },
+        },
+    }
+
+    setCurrentRoom = roomName => {
+        const room = {this.state.rooms[roomName]} 
+        this.setState({room})
+    }
+
     render() {
         return (
             <div className="Main" style={styles}>
-                <Sidebar user={this.props.user} signOut={this.props.signOut} />
-                <Chat user={this.props.user} />
+                <Sidebar user={this.props.user} signOut={this.props.signOut} rooms={this.state.rooms} />
+                <Chat user={this.props.user} room={this.state.room} />
             </div>
         )
     }
